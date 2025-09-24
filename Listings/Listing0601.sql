89f390127435723dca491737fba3fb7395be3639
abhendu123 <abhendu123@gmail.com>
2025-09-24 19:07:40 +0100

    Update Listing0601.sql

--- Listings/Listing0601.sql
+++ Listings/Listing0601.sql
@@ -20,3 +20,26 @@ IF EXISTS (
   WHERE [Product] IS NULL
 ) 
 RAISERROR('Unexpected NULL in dbo.Sales_LOAD.[Product]', 11, 1)  ;
+
+
+--IF EXISTS (SELECT * FROM dbo.Sales_LOAD WHERE CustomerID IS NULL)
+--    RAISERROR('Unexpected NULL in [CustomerID]', 11, 1);
+
+--IF EXISTS (SELECT * FROM dbo.Sales_LOAD WHERE OrderDate IS NULL)
+--    RAISERROR('Unexpected NULL in [OrderDate]', 11, 1);
+
+
+
+--Dynamic SQL to check all columns
+
+--DECLARE @sql NVARCHAR(MAX) = N'';
+
+--SELECT @sql = @sql + 
+--    'IF EXISTS (SELECT 1 FROM dbo.Sales_LOAD WHERE [' + c.name + '] IS NULL)
+--        RAISERROR(''Unexpected NULL in dbo.Sales_LOAD.[' + c.name + ']'', 11, 1);
+--    ' + CHAR(13)
+--FROM sys.columns c
+--WHERE c.object_id = OBJECT_ID('dbo.Sales_LOAD')
+--  AND c.is_nullable = 0; -- only check NOT NULL columns (optional)
+
+--EXEC sp_executesql @sql;
